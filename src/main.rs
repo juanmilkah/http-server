18fa407f2143a7aa373514db10e2880d27f322b5
William Muchui <williammuchui584@gmail.com>
2024-09-15 11:26:21 +0300

    add files, remove 2 requests only delimiter

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use std::io::{prelude::*, BufReader};
 use rust_http_server::Threadpool;
 use std::thread;
 use std::time::Duration;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     //4 threads;
     let pool = Threadpool::new(4);
-    for stream in listener.incoming().take(2){
+    for stream in listener.incoming(){
         let stream = stream.unwrap();
         pool.execute(||{
             handle_connection(stream);
@@ -21,12 +22,13 @@ fn handle_connection(mut stream: TcpStream){
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     
 
-    let (status_line, contents) = match &request_line[..]{
-         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "Hello from Rust http Server!"),
-         "GET /sleep HTTP/1.1" => {thread::sleep(Duration::from_secs(5)); ("HTTP/1.1 200 OK", "Hello from sleep Function!")},
-         _ => ("HTTP/1.1 404 NOT FOUND", "Page not found!")
+    let (status_line, file_name) = match &request_line[..]{
+         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+         "GET /sleep HTTP/1.1" => {thread::sleep(Duration::from_secs(5)); ("HTTP/1.1 200 OK", "hello.html")},
+         _ => ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
+    let contents = fs::read_to_string(file_name).expect("Cannot read file {file_name}");
     let length = contents.len();
     let response = format!(
         "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
